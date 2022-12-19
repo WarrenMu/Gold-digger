5f05e88ec5868417d3014bc09a9eeb0930edbae8
WarrenMu <omnikkotow@gmail.com>
2022-12-19 09:05:49 -0800

    a demo sample of how to... windows or linux

---
+++ main.py
@@ -0,0 +1,21 @@
+from selenium import webdriver 
+from selenium.webdriver.firefox.service import Service
+# from w import FireDriverManager
+# import requests
+# from bs4 import BeautifulSoup as bs
+
+# cdp = "C:\Users\Warren\Desktop\geckodriver\geckodriver.exe"
+# driver = webdriver.Firefox(executable_path=cdp)
+
+driver.get("www.google.com")
+driver.quit()
+
+
+
+
+# URL = "https://www.google.com/"
+# page = requests.get(URL)
+
+# soup = bs(page.content, "html.parser")
+# #print(page.text)
+
