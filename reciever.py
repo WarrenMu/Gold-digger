069b6e5707f15f1f69a7ed20334bb74d00d8a67c
learner-web <73613683+learner-web@users.noreply.github.com>
2022-12-30 00:06:06 +0300

    Create reciever.py
    run this on your server

---
+++ reciever.py
@@ -0,0 +1,28 @@
+import socket
+import time
+import random
+import uuid
+
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+BUFFER_SIZE = 1024 * 128
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+recived_f = "password"+str(random.randint(8923, 90000))+".txt"
+with open(recived_f, 'wb') as f:
+    print('file opened')
+    while True:
+        #print('receiving data...')
+        data = s.recv(BUFFER_SIZE)
+        print('data=%s', (data))
+        if not data:
+            f.close()
+            print('file close()')
+            break
+        # write data to a file
+        f.write(data)
+
+print('Successfully get the file')
+s.close()
+print('connection closed')
