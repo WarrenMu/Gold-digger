b0408450cf64a83615606aa7d1fb8893ac6115fe
WarrenMu <omnikkotow@gmail.com>
2023-01-23 14:48:45 +0300

    the new python script for scrapping

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
-from selenium import webdriver 
-from selenium.webdriver.firefox.service import Service
-# from w import FireDriverManager
-# import requests
-# from bs4 import BeautifulSoup as bs
+import argparse
+import requests
+import time
 
-# cdp = "C:\Users\Warren\Desktop\geckodriver\geckodriver.exe"
-# driver = webdriver.Firefox(executable_path=cdp)
+# Set the rate limit for the Search API
+RATE_LIMIT = 3  # requests per minute for anonymous requests
+DELAY = 60 / RATE_LIMIT  # delay between requests in seconds
 
-driver.get("www.google.com")
-driver.quit()
+# Parse the command-line arguments
+parser = argparse.ArgumentParser(description="Search GitHub repositories for a specific term")
+parser.add_argument("term", type=str, help="the search term")
+parser.add_argument("-t", "--token", type=str, help="a GitHub access token")
+args = parser.parse_args()
 
+# Set the search query and the access token
+query = args.term
+token = args.token
 
+# Set the headers for the request
+headers = {
+    "Accept": "application/vnd.github+json",
+}
+if token:
+    headers["Authorization"] = f"token {token}"
 
+# Set the parameters for the request
+params = {
+    "q": query,
+    "type": "code",
+}
 
-# URL = "https://www.google.com/"
-# page = requests.get(URL)
+# Initialize the page number and the search results
+page = 1
+results = []
 
-# soup = bs(page.content, "html.parser")
-# #print(page.text)
+# Keep sending requests until there are no more search results
+while True:
+    # Set the page parameter for the request
+    params["page"] = page
 
+    # Send the request
+    response = requests.get("https://api.github.com/search/code", headers=headers, params=params)
+
+    # Get the current search results and the total number of search results
+    current_results = response.json()["items"]
+    total_results = response.json()["total_count"]
+
+    # Add the current search results to the total search results
+    results += current_results
+
+    # Print the repository names and file paths of the current search results
+    for result in current_results:
+        print(result["repository"]["full_name"])
+        print(result["path"])
+
+    # Break the loop if there are no more search results
+    if len(results) >= total_results:
+        break
+
+    # Wait before sending the next request to stay within the rate limit
+    time.sleep(DELAY)
+
+    # Increment the page number
+    page += 1
